7f67e153a27c73424047cbc0f5ae0ccd9a6c8517
Adrian333Dev <adrian333dev@gmail.com>
2023-12-03 00:40:41 +0400

    app setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <h1>Welcome to {{ title }}!</h1>
+    <div class="card flex justify-content-center">
+      <p-button label="Check" icon="pi pi-check"></p-button>
+    </div>
     <router-outlet></router-outlet>
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'ngstart-chat';
+  title = 'Angular Start Chat';
 }
